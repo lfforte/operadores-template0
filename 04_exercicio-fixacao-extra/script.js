3f3d67d3a8fc105fc5a05f26e073f7fc6eef77f3
LuizFernandoForte <lfforte@gmail.com>
2023-11-08 14:51:53 -0300

    Resolução Pratica extra

--- 04_exercicio-fixacao-extra/script.js
+++ 04_exercicio-fixacao-extra/script.js
@@ -2,3 +2,9 @@
 
 ///---------------Prática extra - operadores
 
+let suaIdade = Number(prompt("Digite sua idade."));
+let amigoIdade = Number(prompt("Digite a idade de seu amigo."));
+let difIdade = suaIdade - amigoIdade;
+
+console.log("Sua idade é maior que a do seu melhor amigo?", suaIdade>amigoIdade);
+console.log("A diferença de idade é de:", difIdade);
